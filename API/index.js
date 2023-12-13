97c2bc4311971e6356484f926945bb0a5e1dae76
KatynOCtraining <t.theveupolaris@gmail.com>
2023-12-13 18:33:02 +0100

    base back

---
+++ API/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//importe le paquet express
+const express = require("express");
+// crée une app express
+const app = express();
+
+app.listen(5500, () => console.log("server starded : 5500"));
